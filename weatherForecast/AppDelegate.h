315b9ec41dc782d70f0494f71a17b0a6ab2779ba
李龙跃 <longyue_li@suishouji.com>
2017-07-19 16:54:33 +0800

    Initial Commit

---
+++ weatherForecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weatherForecast
+//
+//  Created by RobertLee on 2017/7/19.
+//  Copyright © 2017年 RobertLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weatherForecast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weatherForecast
+//
+//  Created by RobertLee on 2017/7/19.
+//  Copyright © 2017年 RobertLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
